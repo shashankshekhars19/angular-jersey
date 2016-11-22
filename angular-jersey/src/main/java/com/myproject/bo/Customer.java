bdf96c51ece7fa0d2e93e4e871b5e4043c9a2cf1
SHASHANK SHEKHAR <SHASHANK SHEKHAR>
2016-11-22 23:49:29 +0530

    myFirstGit Project

---
+++ angular-jersey/src/main/java/com/myproject/bo/Customer.java
@@ -0,0 +1,59 @@
+package com.myproject.bo;
+
+public class Customer {
+	
+	private String name;
+	private String address;
+	private String phoneNo;
+	private String gender;
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the address
+	 */
+	public String getAddress() {
+		return address;
+	}
+	/**
+	 * @param address the address to set
+	 */
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	/**
+	 * @return the phoneNo
+	 */
+	public String getPhoneNo() {
+		return phoneNo;
+	}
+	/**
+	 * @param phoneNo the phoneNo to set
+	 */
+	public void setPhoneNo(String phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+	/**
+	 * @return the gender
+	 */
+	public String getGender() {
+		return gender;
+	}
+	/**
+	 * @param gender the gender to set
+	 */
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	
+
+}
---
+++ angular-jersey/src/main/java/com/myproject/test/Test.java
@@ -0,0 +1,19 @@
+package com.myproject.test;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.myproject.guice.MyGuiceImpl;
+import com.myproject.interceptor.LogInterCeptor;
+import com.myproject.rest.RestHandler;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		  Injector i= Guice.createInjector(new MyGuiceImpl());
+		  RestHandler rh = i.getInstance(RestHandler.class);
+		  rh.getDetails();
+
+	}
+
+}
